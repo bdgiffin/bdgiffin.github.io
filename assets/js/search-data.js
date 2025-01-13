37b03f8fc6477742f582c270f975cbb48ddb2f97
bdgiffin <bdgiffin@ucdavis.edu>
2025-01-13 18:12:39 +0000

    Deploying to gh-pages from @ bdgiffin/bdgiffin.github.io@010beeaaf438d8b92fea7d9b139627deab8d5818 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
@@ -112,56 +112,21 @@ ninja.data = [{
             },},{id: "news-himamshu-poudel-will-be-delivering-a-presentation-on-the-fragility-analysis-of-transmission-towers-through-physics-based-modeling-of-tornado-induced-wind-and-debris-impact-loads-at-the-upcoming-nheri-computational-symposium",
           title: 'Himamshu Poudel will be delivering a presentation on the Fragility Analysis of Transmission...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-hyper-dimensional-contact-gap-methods",
+          section: "News",},{id: "projects-hyper-dimensional-contact-gap-methods",
           title: 'Hyper-dimensional contact gap methods',
-          description: "a novel finite element contact discretization method",
+          description: "A novel finite element contact discretization method",
           section: "Projects",handler: () => {
               window.location.href = "/projects/hyperdimensional_contact/";
+            },},{id: "projects-time-reversible-computational-methods",
+          title: 'Time-reversible computational methods',
+          description: "Exploration of exactly &quot;bit-reversible&quot; dynamic time-integrators",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/reverse_time_dynamics/";
+            },},{id: "projects-structural-wind-borne-debris-impact-risk-assessment-library-swirl",
+          title: 'Structural Wind-borne debris Impact Risk Assessment Library (SWIRL)',
+          description: "A physics-based fluid-structure-debris modeling framework",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/tornado_debris_impact/";
             },},{
         id: 'social-email',
         title: 'email',
