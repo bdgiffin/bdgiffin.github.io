ad9ada2fce52ca887caf90333fab2a2187f89003
bdgiffin <bdgiffin@ucdavis.edu>
2025-01-14 01:14:04 +0000

    Deploying to gh-pages from @ bdgiffin/bdgiffin.github.io@dc829937a7885c90bf9db0a2ceb2705705b23bee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Recent and ongoing projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
